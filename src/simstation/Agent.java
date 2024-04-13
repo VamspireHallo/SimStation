5dd332eb978b53fefc2d00fb8735d6d31b06106f
VK Vusuwandla <vamseevkv@gmail.com>
2024-04-13 00:44:12 -0700

    Adding code from course site for simulation

--- src/simstation/Agent.java
+++ src/simstation/Agent.java
@@ -23,18 +23,47 @@ public abstract class Agent implements Serializable, Runnable{
     }
 
     // Setup for operations below
-    public void run(){}
-    public void start(){}
-    public void suspend(){
+    public void run(){
+        myThread = Thread.currentThread();
+        while(!stopped) {
+            try{
+                update();
+                Thread.sleep(100);
+                isSuspended();
+
+            }
+            catch(InterruptedException e){
+                Utilities.error(e);
+            }
+        }
+    }
+    public synchronized void start(){
+        myThread = new Thread(this);
+        myThread.start();
+    }
+    public synchronized void suspend(){
         suspended = true;
     }
-    public void resume(){
+    private synchronized void isSuspended(){
+        try{
+            while(!stopped && suspended) {
+                wait();
+                suspended = false;
+            }
+        }
+        catch (InterruptedException e) {
+            Utilities.error(e);
+        }
+    }
+    public synchronized void resume(){
         suspended = false;
     }
-    public void stop(){
+    public synchronized void stop(){
         stopped = true;
     }
     public abstract void update();
-    public void move(int steps){}
+    public synchronized void move(int steps){
+
+    }
 
 }
